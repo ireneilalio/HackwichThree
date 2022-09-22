2622071b60078b92b87f87d78393f9cf0dddf5ea
Irene Ilalio <irenei@hawaii.edu>
2022-09-21 22:43:59 -1000

    Initial Commit

---
+++ Hackwich3UITests/Hackwich3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Hackwich3UITestsLaunchTests.swift
+//  Hackwich3UITests
+//
+//  Created by Irene Ilalio on 9/21/22.
+//
+
+import XCTest
+
+final class Hackwich3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
